115c8fada6027566b2dd20a1698281eb419a3015
ManarElsheikh16 <manar.abdalhafez@compit.aun.edu.eg>
2023-05-28 15:41:37 +0300

    First Commit

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,103 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from '../Custom-Validations/Username.validators';
+import { ConfirmPassVaildators } from '../Custom-Validations/ConfirmPass.vaildators';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  constructor(private FormBuilder:FormBuilder,private router:Router)
+  {}
+
+  defaultSelectedOption:string="Where You Hear About Us";
+  RegisterationForm=this.FormBuilder.group({
+   Username:['',[Validators.pattern(/[A-Z].*$/),Validators.required,Validators.minLength(5),forbiddenNameValidator(/admin/)]],
+   Email:['',[Validators.required,Validators.email]],
+   AlternativeEmails:this.FormBuilder.array([]),
+   Subscribe:[false],
+   Pass:['',[Validators.required]],
+   ConfirmPass:['',[Validators.required]],
+   SelectOptions:['',[Validators.required]],
+   Address:this.FormBuilder.group({
+    City:['',[Validators.required]],
+    State:['',[Validators.required]],
+    PostalCode:['',[Validators.required]],
+   })
+  },{validator:[ConfirmPassVaildators]});
+
+  get Username()
+  {
+    return this.RegisterationForm.get('Username');
+  }
+  get Pass()
+  {
+    return this.RegisterationForm.get('Pass');
+  }
+  get ConfirmPassword()
+  {
+    return this.RegisterationForm.get('ConfirmPass');
+  }
+  get Email()
+  {
+    return this.RegisterationForm.get('Email');
+  }
+  get AlternativeEmails()
+  {
+    return this.RegisterationForm.get('AlternativeEmails') as FormArray;
+  }
+  get SelectOptions(){
+    return this.RegisterationForm.get('SelectOptions');
+
+  }
+  get City()
+  {
+    return this.RegisterationForm.get('City');
+
+  }
+  get State()
+  {
+    return this.RegisterationForm.get('State');
+  }
+  get PostalCode()
+  {
+    return this.RegisterationForm.get('PostalCode');
+  }
+  addAlternativeEmail()
+  {
+    this.AlternativeEmails.push(this.FormBuilder.control(''))
+  }
+  clearEmail(i:any)
+  {
+    this.AlternativeEmails.removeAt(i);
+  }
+  submitData(RegisterationForm:FormGroup)
+  {
+    console.log(RegisterationForm );
+    console.log(this.RegisterationForm.value);
+    this.router.navigate(['/login']);
+
+  }
+
+
+  //conditional validation
+  // setEmailValidation()
+  // {
+  //   this.RegisterationForm.get('Subscribe')?.valueChanges.subscribe(CheckedValue=>{
+  //     if(CheckedValue)
+  //     { //set validation
+  //       this.Email?.setValidators(Validators.required)
+  //     }
+  //     else
+  //     { //clear validation
+  //      this.Email?.clearValidators()
+  //     }
+  //     //update dom
+  //     this.Email?.updateValueAndValidity();
+  //   })
+  // }
+}
